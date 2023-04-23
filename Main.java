3a30cc2dd8dc07a7578abb83940ca00b506dbc90
ShehabAgain <Shehabsalem666@gmail.com>
2023-04-23 19:46:14 +0000

    Added Switch Cases

--- Main.java
+++ Main.java
@@ -14,33 +14,31 @@ class Main {
     System.out.println("Which operator would you like to use?");
     String operation = scanner.nextLine();
 
-    switch (operation){
-      case "sum":  
-        System.out.printf("%n%f+%f=%f",num1,num2,num1+num2);
+    switch (operation) {
+      case "sum":
+        System.out.printf("%n%f+%f=%f", num1, num2, num1 + num2);
         break;
       case "product":
-        System.out.printf("%n%f*%f=%f",num1,num2,num1*num2);
+        System.out.printf("%n%f*%f=%f", num1, num2, num1 * num2);
         break;
       case "quotient":
-          if (num2 ==0){
-            System.out.print("dividing by zero is not possible");
-          } else {
-            System.out.printf("%n%f/%f=%f",num1,num2,num1/num2);
-          }
+        if (num2 == 0) {
+          System.out.print("dividing by zero is not possible");
+        } else {
+          System.out.printf("%n%f/%f=%f", num1, num2, num1 / num2);
+        }
         break;
       case "difference":
-        System.out.printf("%n%f-%f=%f",num1,num2,num1-num2);
+        System.out.printf("%n%f-%f=%f", num1, num2, num1 - num2);
         break;
       case "square":
-        System.out.printf("%n%f^%f=%f",num1,num2,Math.pow(num1,num2));
+        System.out.printf("%n%f^%f=%f", num1, num2, Math.pow(num1, num2));
         break;
       default:
-        System.out.printf("%s is not a supported operation" , operation);
-        
-        
-        
+        System.out.printf("%s is not a supported operation", operation);
+
     }
-   
+
     scanner.close();
   }
 }
